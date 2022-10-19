963fe6798dbf208e47ee097cf6482c87bba60190
mxochicale <miguel.xochicale@kcl.ac.uk>
2022-10-19 09:43:22 +0100

    minor fixes pass pylint 9.82/10 (#1)

--- tests/test_sksurgeryspherefitting_demo.py
+++ tests/test_sksurgeryspherefitting_demo.py
@@ -4,7 +4,11 @@
 
 from scikitsurgeryspherefitting.ui.scikitsurgeryspherefitting_demo import run_demo
 
+
 def test_fit_sphere_least_squares_demo():
+    """
+    Test fit sphere using least squares
+    """
     model_name = 'data/CT_Level_1.vtp'
     output_name = 'out_temp.vtp'
 
